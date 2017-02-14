7f492982df6cae8da4e79c6a0e1fc068737f4c74
Steven Swigart <sbswigart@gmail.com>
2017-02-14 14:42:58 -0800

    simple hash example

---
+++ README.md
@@ -0,0 +1,9 @@
+### Hash Example
+
+Minimal example to demonstrate how we are going to hash images and then use their SHA to store that value in redis and S3.
+
+To run:
+```
+npm install
+node hash-test.js
+```
---
+++ hash-test.js
@@ -0,0 +1,15 @@
+const objectHash = require('object-hash')
+const fs = require('fs')
+
+// contents of images on local file system
+var image01 = fs.readFileSync('test-image-01.jpg')
+var image02 = fs.readFileSync('test-image-02.jpg')
+var nonmatchingImage01 = fs.readFileSync('nonmatchin-image-01.png')
+
+var image01Hash = objectHash.sha1(image01)
+var image02Hash = objectHash.sha1(image02)
+var nonmatchingImage01 = objectHash.sha1(nonmatchingImage01)
+
+console.log(`Matching images should return true: ${image01Hash === image02Hash}`)
+
+console.log(`Nonmatchin images should return false: ${image01Hash === nonmatchingImage01}`)
